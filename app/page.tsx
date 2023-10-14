dd7a54511abe0908f9385f992064efaaec3499c1
김솔지 <so1gging@gmail.com>
2023-10-14 20:59:37 +0900

    안쓰이는 firestore 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
 'use client'
-import { doc, getDoc } from '@firebase/firestore'
-import fireStore from '@/firebase/firestore'
 import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth'
 import fireAuth from '@/firebase/fireAuth'
 
 export default function Home() {
-  const onClickUpLoadButton = async () => {
-    const query = await getDoc(doc(fireStore, '사용자', 'fDilP6RhwRBQnMosqrCe'))
-    console.log(query.data())
-  }
-
   const handleLogin = async () => {
     const provider = new GoogleAuthProvider() // provider 구글 설정
     const data = await signInWithPopup(fireAuth, provider) // 팝업창 띄워서 로그인
@@ -20,7 +13,6 @@ export default function Home() {
   return (
     <main>
       <div>
-        <button onClick={onClickUpLoadButton}>전송</button>
         <button onClick={handleLogin}>로그인</button>
       </div>
     </main>
