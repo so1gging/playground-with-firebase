2794d610c5d8c881b8f22069f95e4434bdad0c7d
김솔지 <so1gging@gmail.com>
2023-10-14 20:31:27 +0900

    로그인 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { doc, getDoc } from '@firebase/firestore'
 import fireStore from '@/firebase/firestore'
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth'
+import fireAuth from '@/firebase/fireAuth'
 
 export default function Home() {
   const onClickUpLoadButton = async () => {
@@ -8,10 +10,18 @@ export default function Home() {
     console.log(query.data())
   }
 
+  const handleLogin = async () => {
+    const provider = new GoogleAuthProvider() // provider 구글 설정
+    const data = await signInWithPopup(fireAuth, provider) // 팝업창 띄워서 로그인
+
+    console.log(data.user)
+  }
+
   return (
     <main>
       <div>
         <button onClick={onClickUpLoadButton}>전송</button>
+        <button onClick={handleLogin}>로그인</button>
       </div>
     </main>
   )
